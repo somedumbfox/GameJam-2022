634644ed9045bd1643ddba77d0c1f43a37ef5b4b
Juan Lopez <Juan.Lopez@kodakmoments.com>
2022-06-02 11:46:49 -0400

    Added volume settings, fixed NPC path direction

--- Game/Levels/Menu/MainMenu.gd
+++ Game/Levels/Menu/MainMenu.gd
@@ -4,23 +4,50 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var BGMvolumeSlider = $CanvasLayer/Settings/BGMVolume/VBoxContainer/BGMVolumeSlider
+onready var SFXvolumeSlider = $CanvasLayer/Settings/SFXVolume/VBoxContainer/SFXVolumeSlider
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CanvasLayer/Settings.hide()
 
 func load_next():
 	get_tree().change_scene("res://Levels/MainLevel/MainLevel.tscn")
 
-func _process(delta):
-	pass
+func open_settings():
+	$CanvasLayer/Settings.show()
+
+func close_settings():
+	$CanvasLayer/Settings.hide()
 
 func _unhandled_input(event):
 	if(event is InputEventScreenTouch):
 		if(event.is_pressed()):
 			load_next()
 
+func set_BGM_Volume(num:float):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), num)
+
+func set_SFX_Volume(num:float):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), num)
 
 func _on_Subtitle_pressed():
 	load_next() # Replace with function body.
+
+
+
+func _on_closeSettings_pressed():
+	close_settings() # Replace with function body.
+
+
+func _on_OpenSettings_pressed():
+	open_settings() # Replace with function body.
+
+
+func _on_BGMVolumeSlider_value_changed(value):
+	set_BGM_Volume(BGMvolumeSlider.value)
+
+
+func _on_SFXVolumeSlider_value_changed(value):
+	set_SFX_Volume(SFXvolumeSlider.value)
